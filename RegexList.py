6980afde20e85dffa91626146b4f6d972a0b2cbb
wayneferdon <wayneferdon@hotmail.com>
2022-10-07 20:24:38 +0800

    Update RegexList.py

--- RegexList.py
+++ RegexList.py
@@ -2,8 +2,8 @@
 # Author: wayneferdon wayneferdon@hotmail.com
 # Date: 2022-10-05 16:15:12
 # LastEditors: wayneferdon wayneferdon@hotmail.com
-# LastEditTime: 2022-10-05 18:16:45
-# FilePath: \Wox.Plugin.Where\RegexList.py
+# LastEditTime: 2022-10-07 20:09:13
+# FilePath: \Wox.Plugin.ChromeBookmarks\RegexList.py
 # ----------------------------------------------------------------
 # Copyright (c) 2022 by Wayne Ferdon Studio. All rights reserved.
 # Licensed to the .NET Foundation under one or more agreements.
@@ -16,14 +16,18 @@ import re
 
 class RegexList:
     def __init__(self, queryString: str):
-        queryStringLower = queryString.lower()
-        queryList = queryStringLower.split()
-        self.regexList = list[re.Pattern]()
-        for query in queryList:
-            self.regexList.append(re.compile(query))
+        self.queryString = queryString
+        queryList = RegexList.__replaceBrackets__(queryString).lower().split()
+        self.__regexs__ = [ re.compile(x) for x in queryList ]
+
+    def __replaceBrackets__(string:str):
+        return string.replace('[', '\[') \
+            .replace(']', '\]') \
+            .replace('(', '\(') \
+            .replace(')', '\)')
 
     def match(self, item: str):
-        match = True
-        for regex in self.regexList:
-            match = regex.search(item.lower()) and match
-        return match
+        for regex in self.__regexs__:
+            if not regex.search(item.lower()):
+                return False
+        return True
